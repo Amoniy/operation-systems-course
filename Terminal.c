9fd34f4eddf1b184ce72255643c6821275f8b2da
Anton Kopitsa <anton17.04.1998@gmail.com>
2018-03-10 19:57:08 +0300

    Starting point

---
+++ Terminal.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char **getLine() {
+    char **strings;
+    while (true) {
+        int currentChar = getchar();
+        if ((currentChar == "\n") || (currentChar == EOF)) {
+            break;
+        }
+//        else if()
+
+    }
+    return strings;
+}
+
+int main(int argc, char *argv[]) {
+    pid_t pid = fork();
+    int status;
+    if (pid == 0) {
+        while (true) {
+            char **strings = getLine();
+            if (execve(argv[1], argv + 1, NULL) < 0) {
+                perror("execve failed");
+            }
+        }
+//        exit(0);
+    } else if (pid > 0) {
+        waitpid(pid, &status, 0);
+    } else {
+        perror("fork failed");
+    }
+    return 0;
+}
